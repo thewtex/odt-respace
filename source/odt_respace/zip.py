179bff578bc7f1ceb0835b4bc496b4f8c9d4d5e2
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-10-03 19:16:00 -0500

    use zlib compression when saving the zip file

--- source/odt_respace/zip.py
+++ source/odt_respace/zip.py
@@ -1,5 +1,5 @@
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 class Zip():
     """Zip up all the contents of a directory into the output file."""
@@ -9,15 +9,15 @@ class Zip():
 
     def zip(self):
       try:
-	zip_file = ZipFile(self.output_file, 'w')
-	for root, dirs, files in os.walk(self.input_directory, topdown=True):
-	  dir_prefix = root[len(os.path.commonprefix([self.input_directory, root]))+1:]
-	  if len(dirs) is 0 and len(files) is 0:
-	    zip_file.write(root, dir_prefix[:-1])
-	  else:
-	    for name in files:
-	      zip_file.write(os.path.join(root, name), os.path.join(dir_prefix, name))
+        zip_file = ZipFile(self.output_file, 'w', ZIP_DEFLATED)
+        for root, dirs, files in os.walk(self.input_directory, topdown=True):
+          dir_prefix = root[len(os.path.commonprefix([self.input_directory, root]))+1:]
+          if len(dirs) is 0 and len(files) is 0:
+            zip_file.write(root, dir_prefix[:-1])
+          else:
+            for name in files:
+              zip_file.write(os.path.join(root, name), os.path.join(dir_prefix, name))
 
       finally:
-	zip_file.close()
+        zip_file.close()
 
